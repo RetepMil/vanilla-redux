486360e0454eb2e37d240d1d31dc0740b024c748
hyeongwoo.lim <lkslyj2@naver.com>
2021-10-04 16:35:04 +0900

    fix: shorten code using redux-toolkit

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { actionCreators } from "../store";
+import { remove } from "../store";
 
 const ToDo = ({ text, id, onBtnClick }) => {
   return (
@@ -13,7 +13,7 @@ const ToDo = ({ text, id, onBtnClick }) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+    onBtnClick: () => dispatch(remove(ownProps.id)),
   };
 };
 
--- src/store.js
+++ src/store.js
@@ -1,31 +1,18 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => [
+      { text: action.payload, id: Date.now() },
+      ...state,
+    ],
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
+  },
+});
 
-const addToDo = (text) => {
-  return { type: ADD, text };
-};
-const deleteToDo = (id) => {
-  return { type: DELETE, id: parseInt(id) };
-};
+export const { add, remove } = toDos.actions;
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
-
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
-
-export default store;
+export default configureStore({ reducer: toDos.reducer });
